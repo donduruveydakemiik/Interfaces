42eb2690fce8917383e6a1928107c1748f7feb47
RÜVEYDA KEMİK <77358367+donduruveydakemiik@users.noreply.github.com>
2023-02-07 16:03:36 +0300

    Add files via upload

---
+++ İnterfaces/İnterfaces/ICostumerDal.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İnterfaces
+{
+    internal interface ICostumerDal //veri tabanı işlemlerini yapmak için kullanıyoruz
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+
+    class SqlServerCostumerDal : ICostumerDal
+    {
+        public void Add()
+        {
+           Console.WriteLine("sql added.");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("sql deleted.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("sql updateded.");
+        }
+
+    }
+
+    class OracleCostumerDal : ICostumerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("oracle added.");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("oracle deleted.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("oracle updateded.");
+        }
+
+    }
+
+    class CustomerManager
+    {
+        public void add(ICostumerDal customerDal)
+        {
+            customerDal.Add();
+            
+        }
+    }
+
+}
---
+++ İnterfaces/İnterfaces/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace İnterfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager=new CustomerManager();
+            customerManager.add(new SqlServerCostumerDal());
+            Console.ReadLine(); 
+        }
+    }
+}
